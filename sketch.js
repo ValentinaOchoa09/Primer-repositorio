b725360df29a6a335ecc5c51cf353124532242e4
Valentina Ochoa <127344361+ValentinaOchoa09@users.noreply.github.com>
2023-03-16 17:55:06 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight).position(0, 0).style("z-index", -1);
+  textSize(25);
+createA('https://gtecom/ValentinaOchoa09', 'Mis avances sesión 1', '_blank').id('Valentina');
+  frameRate(4);
+  background(150, 100, 200);
+}
+
+function draw() {
+  text("🍡", mouseX, mouseY);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function mousePressed () {  
+      background('green');  
+  }
+
