e698a4f948d0c558f037fa11274fcf0f068e32ca
Valentina Ochoa <127344361+ValentinaOchoa09@users.noreply.github.com>
2023-03-16 17:59:04 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,7 +2,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight).position(0, 0).style("z-index", -1);
   textSize(25);
 createA('https://gtecom/ValentinaOchoa09', 'Mis avances sesión 1', '_blank').id('Valentina');
-  frameRate(4);
+  frameRate(6);
   background(150, 100, 200);
 }
 
